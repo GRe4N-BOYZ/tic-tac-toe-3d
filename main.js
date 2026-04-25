834168039848b3629779b4432a1ba42bed3bc70b
Ugallun <yonomo.sasakura@gmail.com>
2026-04-25 21:39:50 +0900

    2d & 3d cube

--- main.js
+++ main.js
@@ -79,3 +79,48 @@ function isDraw() {
 // 初期化
 statusText.textContent = currentPlayer + "'s turn";
 renderBoard();
+
+// ===== ここからThree.jsの追加 =====
+
+//シーン
+const scene = new THREE.Scene();
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1, 1000
+);
+
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+//キューブ生成
+function createCell3D(x, y) {
+    const geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
+    const material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa});
+    const cube = new THREE.Mesh(geometry, material);
+
+    cube.position.x = x - 1; // -1, 0, 1の位置に配置
+    cube.position.y = 1 - y; // 1, 0, -1の位置に配置
+    
+    scene.add(cube);
+}
+
+//配置
+for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+        createCell3D(x, y);
+    }
+}
+
+//描画
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
